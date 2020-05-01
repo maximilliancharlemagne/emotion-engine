a160a161af6eb475628a02e5813fdc1536e41ddf
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-01 08:37:50 -0400

    tested TextRazor -> CORS error

---
+++ assets/javascript/script.js
@@ -0,0 +1,40 @@
+//API Test 1
+//Analyzing some text with TextRazor
+
+//API key: 9e38cdddb8baaef247045e80794aed4c913c1ef8538539bd05fb3de8
+//pls no steal
+
+//textrazor url: https://api.textrazor.com
+
+//important reference info: https://stackoverflow.com/questions/25515936/perform-curl-request-in-javascript
+
+/*
+$.ajax({
+        url: 'https://api.wit.ai/message?v=20140826&q=',
+        beforeSend: function(xhr) {
+             xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
+        }, success: function(data){
+            alert(data);
+            //process the JSON data etc
+        }
+})
+*/
+
+// $.ajax({
+//   type: "POST",
+//   url: 'https://api.textrazor.com',
+//   data: {
+//     text: 'The rain in Spain falls mainly on the plain.'
+//   },
+//   beforeSend: function (xhr) {
+//     xhr.setRequestHeader("x-textrazor-key", "9e38cdddb8baaef247045e80794aed4c913c1ef8538539bd05fb3de8")
+//   }, success: function (data) {
+//     alert(data);
+//     //process the JSON data etc
+//   }
+// })
+
+//returns a CORS error (-_-｡)
+
+//API Test 2:
+//Sentiment analyzing some text with ParallelDots
