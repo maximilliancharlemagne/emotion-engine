67fe9010f7ca4add9b94299e1534d595e6720c57
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-01 09:14:29 -0400

    redacted emotion api key, added mozilla speech code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -42,23 +42,81 @@ $.ajax({
 //API Test 2:
 //Sentiment analyzing some text with ParallelDots
 
-//API key: 2UKOrCNzK8621TicKvum4P5XOmEqPm4hVdJ4NZIt4r8
+//API key: redacted
 
-let dataObject = {
-  api_key: '2UKOrCNzK8621TicKvum4P5XOmEqPm4hVdJ4NZIt4r8',
-  lang_code: 'en',
-  text: `I REALLY HATE MEGADETH'S NEW ALBUM!`
-}
+//uncomment test code with your api key
 
-$.ajax({
-  type: "POST",
-  url: 'https://apis.paralleldots.com/v4/sentiment',
-  data: dataObject, 
-  success: function (data) {
-    console.log(data);
-    //process the JSON data etc
-  }
-})
+// let dataObject = {
+//   api_key: 'your-api-key',
+//   lang_code: 'en',
+//   text: `I REALLY HATE MEGADETH'S NEW ALBUM!`
+// }
+
+// $.ajax({
+//   type: "POST",
+//   url: 'https://apis.paralleldots.com/v4/sentiment',
+//   data: dataObject, 
+//   success: function (data) {
+//     console.log(data);
+//     //process the JSON data etc
+//   }
+// })
 
 //TEST STATUS: PASS
 //Returns a sentiment object!
+
+//Test 3: Google Speech-to-Text API
+
+//url: https://speech.googleapis.com/v1p1beta1.speech
+
+//TEST STATUS: FAIL
+//Confuses Max too much
+
+//Test 4: Mozilla Speech-to-Text API
+
+//Copying the Mozilla code straight up
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+speechRecognitionList.addFromString(grammar, 1);
+
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+var hints = document.querySelector('.hints');
+
+var colorHTML = '';
+colors.forEach(function (v, i, a) {
+  console.log(v, i);
+  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+});
+
+hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
+
+document.body.onclick = function () {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function (event) {
+  var color = event.results[0][0].transcript;
+  diagnostic.textContent = 'Result received: ' + color + '.';
+  bg.style.backgroundColor = color;
+  console.log('Confidence: ' + event.results[0][0].confidence);
+}
+
+recognition.onspeechend = function () {
+  recognition.stop();
+}
